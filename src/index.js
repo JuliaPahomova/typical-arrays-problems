6143edb1cb6639047b76e8cb51d9cc56054758ef
JuliaPahomova <70757343+JuliaPahomova@users.noreply.github.com>
2020-09-30 14:02:27 +0300

    feat: add !array for ternary operator

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 exports.min = function min (array) {
-	return (array.length === 0) ? 0 : Math.min.apply(null, array);
+	return (!array || array.length === 0) ? 0 : Math.min.apply(null, array);
 
 }
 
 exports.max = function max (array) {
-  return (array.length === 0) ? 0 : Math.max.apply(null, array);
+  return (!array || array.length === 0) ? 0 : Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return (array.length === 0) ? 0 :array.reduce((x,y)=> (x+y))/array.length;
+  return (!array || array.length === 0) ? 0 :array.reduce((x,y)=> (x+y))/array.length;
 }
