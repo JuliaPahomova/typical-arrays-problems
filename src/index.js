2131eff90f067337d88daa850d099a83e2aae827
JuliaPahomova <70757343+JuliaPahomova@users.noreply.github.com>
2020-09-30 13:49:58 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-
 exports.min = function min (array) {
-  return 0;
+	return (array.length === 0) ? 0 : Math.min.apply(null, array);
+
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (array.length === 0) ? 0 : Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (array.length === 0) ? 0 :array.reduce((x,y)=> (x+y))/array.length;
 }
